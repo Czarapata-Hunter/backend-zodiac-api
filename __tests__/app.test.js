11b21144a8518342e369e54824e7abbe7c3bd101
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-11-01 13:02:32 -0700

    Zodiac 2 tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,25 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
+const { zodiacs } = require('../lib/zodiacs-data');
+
+describe('zodiacs routes', () => {
+  it('/zodiacs should return list of zodiacs', async () => {
+    const res = await request(app).get('/zodiacs');
+    const expected = zodiacs.map((zodiac) => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  it('/zodiacs/:id should return zodiac detail', async () => {
+    const res = await request(app).get('/zodiacs/1');
+    const aquarius = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    };
+    expect(res.body).toEqual(aquarius);
   });
 });
